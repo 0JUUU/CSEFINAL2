e5abc314dde2bd3e76f93e92668cdba6e5a574e4
YOUNGJU <clleo97@naver.com>
2020-10-09 18:32:18 +0900

    add activity_help.xml, activity_info_func.xml

--- app/src/main/java/com/youngju/csefinal2/MainActivity.java
+++ app/src/main/java/com/youngju/csefinal2/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        myRef.setValue("Hello, World!");
 
         RelativeLayout tmap = (RelativeLayout) findViewById(R.id.Map);
         tMapView = new TMapView(this);
